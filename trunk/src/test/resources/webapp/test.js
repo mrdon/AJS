cff0fc9754bd77280e610c2a2102e939359ac795
aro <aro@atlassian.com>
2009-01-05 04:20:54 +0000

    Add more ajs tests, add default profile to run jetty.
    git-svn-id: https://studio.atlassian.com/svn/AJS@25497 2c54a935-e501-0410-bc05-97a93f6bca70

--- trunk/src/test/resources/webapp/test.js
+++ trunk/src/test/resources/webapp/test.js
@@ -7,6 +7,22 @@ testAjs.addTest("testAjsExists", function () {
 testAjs.addTest("testConsoleExists", function () {
     return (typeof console != "undefined" && console != null);
 });
+testAjs.addTest("testAjsIndexOf", function () {
+    return AJS.indexOf([0,1,2,3,4], 2) == 2;
+});
+testAjs.addTest("testAjsIndexOfFromIndex", function () {
+    return AJS.indexOf([0,1,2,3,4,2], 2, 3) == 5;
+});
+testAjs.addTest("testAjsContains", function () {
+    return AJS.contains(["foo", "bar", "hello", "world"], "hello");
+});
+testAjs.addTest("testAjsParams", function () {
+    return AJS.params.company == "atlassian";
+});
+testAjs.addTest("testAjsParamsList", function () {
+    var products = AJS.params.products;
+    return AJS.contains(products, "confluence");
+});
 
 // atlassian-dialog.js tests
 testAjs.addTest("testDialog", function () {
