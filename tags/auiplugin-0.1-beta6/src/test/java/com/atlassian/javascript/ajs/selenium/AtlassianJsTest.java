6f94a13dca5c6cb9bce6c9e13ed6066bd3a650af
aro <aro@atlassian.com>
2009-02-24 23:30:33 +0000

    [maven-release-plugin]  copy for tag auiplugin-0.1-beta6
    git-svn-id: https://studio.atlassian.com/svn/AJS@29036 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ tags/auiplugin-0.1-beta6/src/test/java/com/atlassian/javascript/ajs/selenium/AtlassianJsTest.java
@@ -0,0 +1,52 @@
+package com.atlassian.javascript.ajs.selenium;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import com.atlassian.javascript.ajs.selenium.client.Client;
+import com.atlassian.javascript.ajs.selenium.client.Configuration;
+import com.atlassian.selenium.SeleniumAssertions;
+
+public class AtlassianJsTest extends TestCase
+{
+    private static Client client = Client.getInstance();
+    private static SeleniumAssertions assertThat = new SeleniumAssertions(client, Configuration.getInstance());
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite();
+        suite.setName(AtlassianJsTest.class.getName());
+
+        client.open("ajs/test.html"); //todo fix context path
+        client.waitForPageToLoad(10000);
+        assertThat.textPresent("Test Page");
+
+        String concatenatedTestNames = client.getEval("window.testAjs.getTestNames()");        
+        String[] methodNames = concatenatedTestNames.split(",");
+        for (String methodName : methodNames)
+        {
+            suite.addTest(new AtlassianJsTest(methodName)); // run one test method
+        }
+
+        return suite;
+    }
+
+    private String methodName;
+
+    public AtlassianJsTest(String methodName)
+    {
+        super();
+        this.methodName = methodName;
+    }
+
+    public String getName()
+    {
+        return methodName;
+    }
+
+    protected void runTest() throws Throwable
+    {
+        // todo extract out context path
+        assertEquals("true", client.getEval("window.testAjs." + methodName + "()"));
+    }
+}
