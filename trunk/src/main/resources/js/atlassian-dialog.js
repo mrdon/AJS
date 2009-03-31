2d97b86e0db922eeaa43b43f44fec2a3498c2455
dbaranovskiy <dbaranovskiy@atlassian.com>
2009-03-31 05:46:17 +0000

    Added comments and method get
    git-svn-id: https://studio.atlassian.com/svn/AJS@31782 2c54a935-e501-0410-bc05-97a93f6bca70

--- trunk/src/main/resources/js/atlassian-dialog.js
+++ trunk/src/main/resources/js/atlassian-dialog.js
@@ -202,8 +202,26 @@ AJS.popup = function (width, height, id) {
             this.item.remove();
         };
     }
+    /**
+     * Moves item left in the hierarchy 
+     * @method moveUp
+     * @method moveLeft
+     * @param step {number} how many items to move, default is 1
+     * @return {object} button
+    */
     Button.prototype.moveUp = Button.prototype.moveLeft = itemMove("left", "button");
+    /**
+     * Moves item right in the hierarchy 
+     * @method moveDown
+     * @method moveRight
+     * @param step {number} how many items to move, default is 1
+     * @return {object} button
+    */
     Button.prototype.moveDown = Button.prototype.moveRight = itemMove("right", "button");
+    /**
+     * Removes item
+     * @method remove
+    */
     Button.prototype.remove = itemRemove("button");
 
     /**
@@ -213,17 +231,26 @@ AJS.popup = function (width, height, id) {
      * @return {string} label, if nothing is passed in
      * @return {object} jQuery button object, if label is passed in
     */
-    Button.prototype.label = function (label) {
-        return this.button.html(label);
+    Button.prototype.html = function (label) {
+        return this.item.html(label);
     };
     /**
-     * Getter and setter of
+     * Getter and setter of onclick event handler
+     * @method onclick
+     * @param onclick {function} [optional] new event handler, that is going to replace the old one
+     * @return {function} existing event handler if new one is undefined
     */
     Button.prototype.onclick = function (onclick) {
         if (typeof onclick == "undefined") {
             return this.onclick;
         } else {
-            this.button.unbind("click", this._onclick);
+            this.item.unbind("click", this._onclick);
+            this._onclick = function () {
+                onclick.call(this, page.dialog, page);
+            };
+            if (typeof onclick == "function") {
+                this.item.click(this._onclick);
+            }
         }
     };
 
@@ -235,6 +262,7 @@ AJS.popup = function (width, height, id) {
      * @param title {string} panel title
      * @param reference {string} or {object} jQuery object or selector for the contents of the Panel
      * @param className {string} [optional] HTML class name
+     * @private
     */
     var Panel = function (page, title, reference, className) {
         if (!(reference instanceof AJS.$)) {
@@ -291,36 +319,32 @@ AJS.popup = function (width, height, id) {
         this.button.click();
     };
     /**
-     * Moves panel up in the hierarchy
+     * Moves item left in the hierarchy 
      * @method moveUp
-     * @return object
-    */
-    /**
-     * Moves panel left in the hierarchy
      * @method moveLeft
-     * @return object
+     * @param step {number} how many items to move, default is 1
+     * @return {object} panel
     */
     Panel.prototype.moveUp = Panel.prototype.moveLeft = itemMove("left", "panel");
     /**
-     * Moves panel down in the hierarchy
+     * Moves item right in the hierarchy 
      * @method moveDown
-     * @return object
-    */
-    /**
-     * Moves panel right in the hierarchy
      * @method moveRight
-     * @return object
+     * @param step {number} how many items to move, default is 1
+     * @return {object} panel
     */
     Panel.prototype.moveDown = Panel.prototype.moveRight = itemMove("right", "panel");
     /**
-     * Removes panel
+     * Removes item
      * @method remove
     */
     Panel.prototype.remove = itemRemove("panel");
     /**
-     * Getter and setter for HTML contents of the panel
+     * Getter and setter of inner HTML of the panel
      * @method html
-     * @return object | string
+     * @param html {string} HTML source to set up
+     * @return {object} panel
+     * @return {string} current HTML source
     */
     Panel.prototype.html = function (html) {
         if (html) {
@@ -334,7 +358,7 @@ AJS.popup = function (width, height, id) {
      * Default padding is 10 px. This method gives you ability to overwrite default value. Use it with caution.
      * @method setPadding
      * @param padding {number} padding in pixels
-     * @return object
+     * @return {object} panel
     */
     Panel.prototype.setPadding = function (padding) {
         if (!isNaN(+padding)) {
@@ -345,8 +369,15 @@ AJS.popup = function (width, height, id) {
         return this;
     };
 
-
-
+    
+    /**
+     * Class for pages
+     * @class Page
+     * @constructor
+     * @param dialog {object} dialog object
+     * @param className {string} [optional] HTML class name
+     * @private
+    */
     var Page = function (dialog, className) {
         this.dialog = dialog;
         this.id = dialog.page.length;
@@ -363,6 +394,10 @@ AJS.popup = function (width, height, id) {
         dialog.popup.element.append(this.element.append(this.menu).append(this.body));
         dialog.page[dialog.page.length] = this;
     };
+    /**
+     * Size updater for contents of the page. For internal use
+     * @method recalcSize
+    */
     Page.prototype.recalcSize = function () {
         var headerHeight = this.header ? 43 : 0;
         var panelHeight = this.buttonpanel ? 43 : 0;
@@ -370,46 +405,99 @@ AJS.popup = function (width, height, id) {
             this.panel[i].body.css("height", this.dialog.height - headerHeight - panelHeight - this.panel[i].padding * 2 + "px");
         }
     };
-
+    /**
+     * Method for adding new panel to the page
+     * @method addPanel
+     * @param title {string} panel title
+     * @param reference {string} or {object} jQuery object or selector for the contents of the Panel
+     * @param className {string} [optional] HTML class name
+     * @return {object} the page
+    */
     Page.prototype.addPanel = function (title, reference, className) {
         new Panel(this, title, reference, className);
         return this;
     };
+    /**
+     * Method for adding header to the page
+     * @method addHeader
+     * @param title {string} panel title
+     * @param className {string} [optional] HTML class name
+     * @return {object} the page
+    */
     Page.prototype.addHeader = function (title, className) {
         if (this.header) {
             this.header.remove();
         }
-            this.header = AJS("h2").html(title);
+        this.header = AJS("h2").html(title);
         className && this.header.addClass(className);
         this.element.prepend(this.header);
         this.recalcSize();
         return this;
     };
+    /**
+     * Method for adding new button to the page
+     * @method addButton
+     * @param label {string} button label
+     * @param onclick {function} [optional] click event handler
+     * @param className {string} [optional] class name
+     * @return {object} the page
+    */
     Page.prototype.addButton = function (label, onclick, className) {
         new Button(this, label, onclick, className);
         this.recalcSize();
         return this;
     };
+    /**
+     * Selects corresponding panel
+     * @method gotoPanel
+     * @param panel {object} panel object
+     * @param panel {number} id of the panel
+    */
     Page.prototype.gotoPanel = function (panel) {
         this.panel[panel.id || panel].select();
     };
+    /**
+     * Returns current panel on the page
+     * @method getCurrentPanel
+     * @return panel {object} the panel
+    */
     Page.prototype.getCurrentPanel = function () {
         return this.panel[this.curtab];
     };
+    /**
+     * Hides the page
+     * @method hide
+    */
     Page.prototype.hide = function () {
         this.element.hide();
     };
+    /**
+     * Shows the page, if it was hidden
+     * @method show
+    */
     Page.prototype.show = function () {
         this.element.show();
     };
+    /**
+     * Removes the page
+     * @method remove
+    */
     Page.prototype.remove = function () {
         this.element.remove();
     };
 
 
 
-
-    // Dialog
+    /**
+     * Class for dialog
+     * @class Dialog
+     * @namespace AJS
+     * @constructor
+     * @param width {number} dialog width in pixels
+     * @param height {number} dialog height in pixels
+     * @param id {number} [optional] dialog id
+     * @private
+    */
     AJS.Dialog = function (width, height, id) {
         this.height = height || 480;
         this.width = width || 640;
@@ -421,24 +509,58 @@ AJS.popup = function (width, height, id) {
         this.curpage = 0;
         new Page(this);
     };
+    /**
+     * Method for adding header to the current page
+     * @method addHeader
+     * @param title {string} panel title
+     * @param className {string} [optional] HTML class name
+     * @return {object} the dialog
+    */
     AJS.Dialog.prototype.addHeader = function (title, className) {
         this.page[this.curpage].addHeader(title, className);
         return this;
     };
+    /**
+     * Method for adding new button to the current page
+     * @method addButton
+     * @param label {string} button label
+     * @param onclick {function} [optional] click event handler
+     * @param className {string} [optional] class name
+     * @return {object} the dialog
+    */
     AJS.Dialog.prototype.addButton = function (label, onclick, className) {
         this.page[this.curpage].addButton(label, onclick, className);
         return this;
     };
+    /**
+     * Method for adding new panel to the current page
+     * @method addPanel
+     * @param title {string} panel title
+     * @param reference {string} or {object} jQuery object or selector for the contents of the Panel
+     * @param className {string} [optional] HTML class name
+     * @return {object} the dialog
+    */
     AJS.Dialog.prototype.addPanel = function (title, reference, className) {
         this.page[this.curpage].addPanel(title, reference, className);
         return this;
     };
+    /**
+     * Method for adding new page
+     * @method addPage
+     * @param className {string} [optional] HTML class name
+     * @return {object} the dialog
+    */
     AJS.Dialog.prototype.addPage = function (className) {
         new Page(this, className);
         this.page[this.curpage].hide();
         this.curpage = this.page.length - 1;
         return this;
     };
+    /**
+     * Making next page in hierarchy visible and active
+     * @method nextPage
+     * @return {object} the dialog
+    */
     AJS.Dialog.prototype.nextPage = function () {
         this.page[this.curpage++].hide();
         if (this.curpage >= this.page.length) {
@@ -447,6 +569,11 @@ AJS.popup = function (width, height, id) {
         this.page[this.curpage].show();
         return this;
     };
+    /**
+     * Making previous page in hierarchy visible and active
+     * @method prevPage
+     * @return {object} the dialog
+    */
     AJS.Dialog.prototype.prevPage = function () {
         this.page[this.curpage--].hide();
         if (this.curpage < 0) {
@@ -455,6 +582,12 @@ AJS.popup = function (width, height, id) {
         this.page[this.curpage].show();
         return this;
     };
+    /**
+     * Making specified page visible and active
+     * @method gotoPage
+     * @param num {number} page id
+     * @return {object} the dialog
+    */
     AJS.Dialog.prototype.gotoPage = function (num) {
         this.page[this.curpage].hide();
         this.curpage = num;
@@ -466,6 +599,13 @@ AJS.popup = function (width, height, id) {
         this.page[this.curpage].show();
         return this;
     };
+    /**
+     * Returns specified panel at the current page
+     * @method getPanel
+     * @param pageorpanelId {number} page id or panel id
+     * @param panelId {number} panel id
+     * @return {object} the panel
+    */
     AJS.Dialog.prototype.getPanel = function (pageorpanelId, panelId) {
         var pageid = (panelId == null) ? this.curpage : pageorpanelId;
         if (panelId == null) {
@@ -473,13 +613,31 @@ AJS.popup = function (width, height, id) {
         }
         return this.page[pageid].panel[panelId];
     };
+    /**
+     * Returns specified page
+     * @method getPage
+     * @param pageid {number} page id
+     * @return {object} the page
+    */
     AJS.Dialog.prototype.getPage = function (pageid) {
         return this.page[pageid];
     };
+    /**
+     * Returns current panel at the current page
+     * @method getCurrentPanel
+     * @return {object} the panel
+    */
     AJS.Dialog.prototype.getCurrentPanel = function () {
         return this.page[this.curpage].getCurrentPanel();
     };
 
+    /**
+     * Selects corresponding panel
+     * @method gotoPanel
+     * @param pageorpanel {object} panel object or page object
+     * @param panel {object} panel object
+     * @param panel {number} id of the panel
+    */
     AJS.Dialog.prototype.gotoPanel = function (pageorpanel, panel) {
         if (panel != null) {
             var pageid = pageorpanel.id || pageorpanel;
@@ -488,16 +646,97 @@ AJS.popup = function (width, height, id) {
         this.page[this.curpage].gotoPanel(typeof panel == "undefined" ? pageorpanel : panel);
     };
 
+    /**
+     * Shows the dialog, if it is not visible
+     * @method hide
+    */
     AJS.Dialog.prototype.show = function () {
         this.popup.show();
         return this;
     };
+    /**
+     * Hides the dialog, if it was visible
+     * @method hide
+    */
     AJS.Dialog.prototype.hide = function () {
         this.popup.hide();
         return this;
     };
+    /**
+     * Removes the dialog
+     * @method remove
+    */
     AJS.Dialog.prototype.remove = function () {
         this.popup.hide();
         this.popup.remove();
+    };
+    /**
+     * Gets set of items depending on query
+     * @method get
+     * @param query {string} query to search for panels, pages, headers or buttons
+    */
+    AJS.Dialog.prototype.get = function (query) {
+        var coll = [],
+            dialog = this;
+        (query + "").replace(/(?:,|^)\s*(?:(page|panel|button|header)(?:#([^ ]*)|:(\d+))?|#([^ ]*))(?:\s+(?:(page|panel|button|header)(?:#([^ ]*)|:(\d+))?|#([^ ]*)))?\s*(?=,|$)/ig, function (all, name, title, id, justtitle, name2, title2, id2, justtitle2) {
+            name = name && name.toLowerCase();
+            var pages = [];
+            if (name == "page" && dialog.page[id]) {
+                pages.push(dialog.page[id]);
+                name = name2;
+                name = name && name.toLowerCase();
+                title = title2;
+                id = id2;
+                justtitle = justtitle2;
+            } else {
+                pages = dialog.page;
+            }
+            if (name || justtitle) {
+                for (var i = pages.length; i--;) {
+                    if (justtitle || (name == "panel" && (title || (!title && id == null)))) {
+                        for (var j = pages[i].panel.length; j--;) {
+                            if (pages[i].panel[j].button.html() == justtitle || pages[i].panel[j].button.html() == title || (name == "panel" && !title && id == null)) {
+                                coll.push(pages[i].panel[j]);
+                            }
+                        }
+                    }
+                    if (justtitle || (name == "button" && (title || (!title && id == null)))) {
+                        for (var j = pages[i].button.length; j--;) {
+                            if (pages[i].button[j].item.html() == justtitle || pages[i].button[j].item.html() == title || (name == "button" && !title && id == null)) {
+                                coll.push(pages[i].button[j]);
+                            }
+                        }
+                    }
+                    if (pages[i][name] && pages[i][name][id]) {
+                        coll.push(pages[i][name][id]);
+                    }
+                    if (name == "header" && pages[i].header) {
+                        coll.push(pages[i].header);
+                    }
+                }
+            } else {
+                coll = coll.concat(pages);
+            }
+        });
+        var res = {
+            length: coll.length
+        };
+        for (var i = coll.length; i--;) {
+            res[i] = coll[i];
+            for (var method in coll[i]) {
+                if (!(method in res)) {
+                    (function (m) {
+                        res[m] = function () {
+                            for (var j = this.length; j--;) {
+                                if (typeof this[j][m] == "function") {
+                                    this[j][m].apply(this[j], arguments);
+                                }
+                            }
+                        };
+                    })(method);
+                }
+            }
+        }
+        return res;
     };
 })();
