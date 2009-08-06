1cba89835e4da4c7b4da45e90c65c19725506a66
mryall <mryall@atlassian.com>
2009-08-06 07:14:54 +0000

    [maven-scm] copy for tag auiplugin-1.1.4
    git-svn-id: https://studio.atlassian.com/svn/AJS@41345 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ tags/auiplugin-1.1.4/src/test/resources/webapp/ajstest.js
@@ -0,0 +1,14 @@
+AJSTest = function () {};
+AJSTest.prototype.addTest = function (name, test) {
+    if (typeof test == "function") {
+        if (name in this) {
+            throw new Error("Test with name “" + name + "” already exists.");
+        }
+        this[name] = test;
+        this.names = this.names || [];
+        this.names.push(name);
+    }
+};
+AJSTest.prototype.getTestNames = function () {
+    return this.names;
+};
