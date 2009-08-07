664a51db70cf04b6b86fd35f646b4c516645f973
mryall <mryall@atlassian.com>
2009-08-07 06:34:16 +0000

    [maven-scm] copy for tag auiplugin-1.1.5
    git-svn-id: https://studio.atlassian.com/svn/AJS@41452 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ tags/auiplugin-1.1.5/src/main/resources/js/jquery/jquery-ui-1.7-bug-fixes.js
@@ -0,0 +1,10 @@
+// fix nested draggables in IE - http://dev.jqueryui.com/ticket/4333
+$.ui.draggable.prototype._mouseCapture = (function (orig) {
+    return function (event) {
+        var result = orig.call(this, event);
+        if (result && $.browser.msie) {
+            event.stopPropagation();
+        }
+        return result;
+    };
+})($.ui.draggable.prototype._mouseCapture);
