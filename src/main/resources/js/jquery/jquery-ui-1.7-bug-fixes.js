79b186da269431545da45196a0ba43b85d2c7564
mryall <mryall@atlassian.com>
2009-08-11 08:44:20 +0000

    AJS-120, AJS-123: Fix reliance on jQuery having the $ global variable
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@41613 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/main/resources/js/jquery/jquery-ui-1.7-bug-fixes.js
+++ src/main/resources/js/jquery/jquery-ui-1.7-bug-fixes.js
@@ -1,10 +1,10 @@
 // fix nested draggables in IE - http://dev.jqueryui.com/ticket/4333
-$.ui.draggable.prototype._mouseCapture = (function (orig) {
+jQuery.ui.draggable.prototype._mouseCapture = (function (orig) {
     return function (event) {
         var result = orig.call(this, event);
-        if (result && $.browser.msie) {
+        if (result && jQuery.browser.msie) {
             event.stopPropagation();
         }
         return result;
     };
-})($.ui.draggable.prototype._mouseCapture);
+})(jQuery.ui.draggable.prototype._mouseCapture);
