10b32e51a44df901ec7956c44d84d94117cebe75
lhardy <lhardy@atlassian.com>
2009-09-08 23:52:12 +0000

    testDialogPosition on fails on my laptop - adding it back in and raising an issue for investigation
    git-svn-id: https://studio.atlassian.com/svn/AJS@43531 2c54a935-e501-0410-bc05-97a93f6bca70

--- branches/aui-1.1.x/src/test/resources/webapp/test.js
+++ branches/aui-1.1.x/src/test/resources/webapp/test.js
@@ -182,16 +182,16 @@ testAjs.addTest("testDialogMenuHidden", function() {
     popup.remove();
     return res;
 });
-// testAjs.addTest("testDialogPosition", function() {
-//     document.documentElement.scrollTop = 10000;
-//     // tests that the dialog menu is hidden when there is only one panel
-//     var popup = new AJS.Dialog(860, 530, "test-dialog");
-//     popup.addPanel("First", "<p></p>", "panel1").show();
-//     
-//     var res = $("#test-dialog").offset().top > document.documentElement.scrollTop;
-//     popup.remove();
-//     return res;
-// });
+testAjs.addTest("testDialogPosition", function() {
+    document.documentElement.scrollTop = 10000;
+    // tests that the dialog menu is hidden when there is only one panel
+    var popup = new AJS.Dialog(860, 530, "test-dialog");
+    popup.addPanel("First", "<p></p>", "panel1").show();
+    
+    var res = $("#test-dialog").offset().top > document.documentElement.scrollTop;
+    popup.remove();
+    return res;
+});
 
 // todo - add more tests here
 
