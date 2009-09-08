6ca29bdc0c6feed5887e0b9fb20c94c87c941d35
lhardy <lhardy@atlassian.com>
2009-09-08 05:00:43 +0000

    AJS-143: Dmitry and Craig's revisions from CR-AJS-56
    git-svn-id: https://studio.atlassian.com/svn/AJS/branches/aui-1.1.x@43432 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/main/resources/js/cookie.js
+++ src/main/resources/js/cookie.js
@@ -15,100 +15,55 @@
 
     function getValueFromConglomerate(name, cookieValue) {
         // a null cookieValue is just the first time through so create it
-        if(cookieValue == null) {
-            cookieValue = "";
-        }
-        var eq = name + "=";
-        var cookieParts = cookieValue.split('|');
-        for(var i = 0; i < cookieParts.length; i++) {
-            var cp = cookieParts[i];
-            while (cp.charAt(0)==' ') {
-                cp = cp.substring(1,cp.length);
-            }
-            // rebuild the value string exluding the named portion passed in
-            if (cp.indexOf(name) == 0) {
-                return cp.substring(eq.length, cp.length);
-            }
-        }
-        return null;
+        cookieValue = cookieValue || "";
+        var reg = new RegExp(name + "=([^|]+)"),
+            res = cookieValue.match(reg);
+        return res && res[1];
     }
 
     //either append or replace the value in the cookie string
-    function addOrAppendToValue(name, value, cookieValue)
-    {
-        var newCookieValue = "";
-        // a null cookieValue is just the first time through so create it
-        if(cookieValue == null) {
-            cookieValue = "";
-        }
+    function addOrAppendToValue(name, value, cookieValue) {
+        var reg = new RegExp("\\s*" + name + "=[^|]+(\\||$)");
 
-        var cookieParts = cookieValue.split('|');
-        for(var i = 0; i < cookieParts.length; i++) {
-            var cp = cookieParts[i];
-
-            // ignore any empty tokens
-            if(cp != "") {
-                while (cp.charAt(0)==' ') {
-                    cp = cp.substring(1,cp.length);
-                }
-                // rebuild the value string exluding the named portion passed in
-                if (cp.indexOf(name) != 0) {
-                    newCookieValue += cp + "|";
-                }
-            }
-        }
-
-        // always append the value passed in if it is not null or empty
-        if(value != null && value != '') {
+        cookieValue = cookieValue || "";
+        cookieValue = cookieValue.replace(reg, "") + (cookieValue ? "|" : "");
+        
+        if (value) {
             var pair = name + "=" + value;
-            if((newCookieValue.length + pair.length) < 4020) {
-                newCookieValue += pair;
+            if (cookieValue.length + pair.length < 4020) {
+                cookieValue += pair;
             }
         }
-        return newCookieValue;
+        return cookieValue;
     }
 
     function getCookieValue(name) {
-        var
-        eq = name + "=",
-        ca = document.cookie.split(';');
-
-        for(var i=0;i<ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') {
-                c = c.substring(1,c.length);
-            }
-            if (c.indexOf(eq) == 0) {
-                return c.substring(eq.length,c.length);
-            }
-        }
-
-        return null;
+        var reg = new RegExp(name + "=([^;]+)"),
+            res = document.cookie.match(reg);
+        return res && res[1];
     }
 
     function saveCookie(name, value, days) {
-      var ex;
+      var ex = "";
       if (days) {
-        var d = new Date();
-        d.setTime(d.getTime()+(days*24*60*60*1000));
-        ex = "; expires="+d.toGMTString();
-      } else {
-        ex = "";
+          var d = new Date();
+          d.setTime(+d + days * 24 * 60 * 60 * 1000);
+          ex = "; expires=" + d.toGMTString();
       }
       document.cookie = name + "=" + value + ex + ";path=/";
     }
 
     AJS.Cookie = {
-        save : function (name, value) {
+        save : function (name, value, expires) {
             var cookieValue = getCookieValue(COOKIE_NAME);
             cookieValue = addOrAppendToValue(name, value, cookieValue);
-            saveCookie(COOKIE_NAME, cookieValue, 365);
+            saveCookie(COOKIE_NAME, cookieValue, expires || 365);
         },
 
         read : function(name, defaultValue) {
             var cookieValue = getCookieValue(COOKIE_NAME);
             var value = getValueFromConglomerate(name, cookieValue);
-            if(value != null) {
+            if (value != null) {
                 return value;
             }
             return defaultValue;
