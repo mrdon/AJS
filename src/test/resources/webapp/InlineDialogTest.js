d73c423a769235f7c33f3c74b7efe7fb979b4b1f
lhardy <lhardy@atlassian.com>
2009-11-18 05:52:34 +0000

    AJS-162: updating tests to use only AJs.$
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@48063 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/test/resources/webapp/InlineDialogTest.js
+++ src/test/resources/webapp/InlineDialogTest.js
@@ -1,8 +1,8 @@
 testAjs.addTest("testInlineDialog", function() {
-    var testContainer = $("#test-container");
-    var link = $("<a>").text("Test Link").appendTo(testContainer);
+    var testContainer = AJS.$("#test-container");
+    var link = AJS.$("<a>").text("Test Link").appendTo(testContainer);
     var dlg = new AJS.InlineDialog(link, 1, "content.html");
-    res = ($("#inline-dialog-1").length == 1);
+    res = (AJS.$("#inline-dialog-1").length == 1);
     testContainer.empty();
     return res;
 });
