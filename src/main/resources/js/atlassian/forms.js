e1a40f594480228dc2705959d5e352490fda5ed5
mjopson <mjopson@atlassian.com>
2010-02-25 04:45:38 +0000

    AJS-241 Forms inline-help component now using siblings() Removed/reformatted whitespace
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@55909 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/main/resources/js/atlassian/forms.js
+++ src/main/resources/js/atlassian/forms.js
@@ -12,13 +12,11 @@
 
 AJS.inlineHelp = function () {
     AJS.$(".icon-inline-help").click(function(){
-        var $next = AJS.$(this).next();
-        if($next.hasClass("field-help")){
-            if ($next.hasClass("hidden")){
-                $next.removeClass("hidden");
-            } else {
-                $next.addClass("hidden");
-            }
+        var $t = AJS.$(this).siblings(".field-help");
+        if ($t.hasClass("hidden")){
+            $t.removeClass("hidden");
+        } else {
+            $t.addClass("hidden");
         }
     });
 };
