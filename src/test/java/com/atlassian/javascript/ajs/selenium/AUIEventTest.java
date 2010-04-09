a8830ba580bb9b469bd04fc5f05ad7fbaa2ec304
mjensen <mjensen@atlassian.com>
2010-04-09 01:29:46 +0000

    AJS-297 * More investigations in to why this test is failing IE+selenium+remote agent
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@58105 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/test/java/com/atlassian/javascript/ajs/selenium/AUIEventTest.java
+++ src/test/java/com/atlassian/javascript/ajs/selenium/AUIEventTest.java
@@ -15,7 +15,7 @@ public class AUIEventTest extends AUISeleniumTestCase
      */
     public void testAJSeventBindsAndTriggers()
     {
-        client.getEval("window.AJS.bind('abc-event', function(data) { testResult = true; });");
+        client.getEval("window.AJS.bind('abc-event', function(data) { window.testResult = true; });");
         client.getEval("window.AJS.trigger('abc-event');");
         assertEquals("Trigger should be fired", "true", client.getEval("window.testResult"));
     }
@@ -25,7 +25,7 @@ public class AUIEventTest extends AUISeleniumTestCase
      */
     public void testAJSeventBindsAndTriggersWithUnbind()
     {
-        client.getEval("window.testResult = 0; window.AJS.bind('abc-event', function(data) { testResult++; });");
+        client.getEval("window.testResult = 0; window.AJS.bind('abc-event', function(data) { window.testResult++; });");
         client.getEval("window.AJS.trigger('abc-event');");
         assertEquals("Trigger should be fired", "1", client.getEval("window.testResult"));
         client.getEval("window.AJS.unbind('abc-event');");
