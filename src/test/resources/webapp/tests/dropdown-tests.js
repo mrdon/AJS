56a69c20c1efe27697e2bab49d529b2aa4b7fb6a
bwong <bwong@atlassian.com>
2010-04-13 06:40:33 +0000

    AJS-271 - Removing obsolete js tests
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@58266 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/test/resources/webapp/tests/dropdown-tests.js
+++ src/test/resources/webapp/tests/dropdown-tests.js
@@ -3,17 +3,3 @@
 testAjs.addTest("testAJSdropdownExists", function () {
     return (typeof AJS.dropDown != "undefined" && AJS.dropDown != null);
 });
-
-testAjs.addTest("testAJSdropdownShows", function () {
-    var ddParent = AJS.$(".aui-dd-parent"),
-        trigger = AJS.$(".aui-dd-trigger:first"),
-        res;
-
-    ddParent.dropDown();
-    trigger.click(function () {
-        ddParent.show();
-    }).trigger("click");
-
-    res = (AJS.$(".aui-dropdown:visible").length != 0);
-    return res;
-});
--- src/test/resources/webapp/tests/inline-dialog-tests.js
+++ src/test/resources/webapp/tests/inline-dialog-tests.js
@@ -4,13 +4,3 @@ testAjs.addTest("testAJSInlineDialogExists", function () {
     return (typeof AJS.InlineDialog != "undefined" && AJS.InlineDialog != null);
 });
 
-testAjs.addTest("testInlineDialog", function() {
-    var testContainer = AJS.$("#test-container");
-    var link = AJS.$("<a>").text("Test Link").appendTo(testContainer);
-    var dlg = new AJS.InlineDialog(link, 1, "content.html");
-    link.trigger("click");
-    res = (AJS.$("#inline-dialog-1").length == 1);
-    testContainer.empty();
-    return res;
-});
-
