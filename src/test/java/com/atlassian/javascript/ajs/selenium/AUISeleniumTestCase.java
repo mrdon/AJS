e872b0c242044d67e92a36cb211ae1323a15ab77
bwong <bwong@atlassian.com>
2010-04-19 07:31:56 +0000

    AJS-291 - added test class for Dialog and replaced failing js test with selenium test.
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@58477 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/test/java/com/atlassian/javascript/ajs/selenium/AUISeleniumTestCase.java
+++ src/test/java/com/atlassian/javascript/ajs/selenium/AUISeleniumTestCase.java
@@ -1,8 +1,8 @@
 package com.atlassian.javascript.ajs.selenium;
 
+import com.atlassian.javascript.ajs.selenium.client.Configuration;
 import com.atlassian.selenium.SeleniumConfiguration;
 import com.atlassian.selenium.SeleniumTest;
-import com.atlassian.javascript.ajs.selenium.client.Configuration;
 
 public class AUISeleniumTestCase extends SeleniumTest
 {
@@ -11,13 +11,21 @@ public class AUISeleniumTestCase extends SeleniumTest
         return Configuration.getInstance();
     }
 
-    public void openTestPage(String filename){
+    public void openTestPage(String filename)
+    {
         client.open(filename);
     }
 
-    public void addHTMLtoElement(String element, String html){
+    public void addHTMLtoElement(String element, String html)
+    {
         html = html.replaceAll("\n", "");
-        client.getEval("window.AJS.$('"+element+"').append('"+ html + "')");
+        client.getEval("window.AJS.$('" + element + "').append('" + html + "')");
+    }
+
+    public void runMultiLineJavascript(String runString)
+    {
+        runString = runString.replaceAll("\n", "");
+        client.getEval(runString);
     }
 
 }
