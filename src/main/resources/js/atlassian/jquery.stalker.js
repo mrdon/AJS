32d38dafa5ea6faa7b7da1beb12c6a34db88a575
sharwood <sharwood@atlassian.com>
2010-04-22 02:00:30 +0000

    AJS-317: Updating stalker to work with jQuery 1.4.2
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@58647 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/main/resources/js/atlassian/jquery.stalker.js
+++ src/main/resources/js/atlassian/jquery.stalker.js
@@ -1,28 +1,28 @@
 /**
- * @module jQuery plugins
+ *
+ * @module Controls
  * @requires AJS, jQuery, jQuery.fn.offsetAnchors
- * @since 2.2
  */
 
 /**
  *
  * When scrolled past, attaches specified element to the top of the screen.
  *
+ *
+ * <pre>
  * <strong>Usage:</strong>
- * <code>
  * jQuery("#stalker").stalker();
- * </code>
+ * </pre>
  *
  * @class stalker
+ *
  * @constuctor stalker
  * @namespace jQuery.fn
  */
 
 jQuery.fn.stalker = function (){
 
-    var DETACHED_CLASS = "detached",
-        PLACEHOLDER_CLASS = "stalker-placeholder",
-        $win, /* jQuery wrapped window */
+    var $win, /* jQuery wrapped window */
         $doc, /* jQuery wrapped document */
         $stalker, /* Element that will follow user scroll (Stalk) */
         $transitionElems, /* Elements preceding stalker */
@@ -33,15 +33,15 @@ jQuery.fn.stalker = function (){
         isInitialized, /* Flag if control is initialized (onscroll) */
         selector; /* Selector for stalker */
 
+
     function isSupported() {
-        if (jQuery.browser.msie && jQuery.browser.version < 7) {
+        if (jQuery.browser.msie && jQuery.browser.version < 7 ||  (!jQuery.os.mac && getBrowserVersionAsInt() < 191)) {
             return false;
-        };
+        }
         return true;
     }
 
     function getInactiveProperties () {
-
         if (jQuery.os.windows || jQuery.os.linux) {
             return {
                 position: "absolute",
@@ -55,6 +55,17 @@ jQuery.fn.stalker = function (){
         }
     }
 
+    function getBrowserVersionAsInt() {
+        return parseInt(jQuery.browser.version.replace(/\.*/g,"").substring(0,3), 10);
+    }
+
+    /* In firefox 3.5 and below all block level elements need to have overflow: auto css property when fixed positioned
+       to avoid flickering when scrolling. */
+    function needToApplyFlickerFix () {
+        var browserVersion = jQuery.browser.version.replace(/\.*/g,"").substring(0,3);
+        return jQuery.browser.mozilla && browserVersion < 192;
+    }
+
     function initialize() {
 
         $stalker = jQuery(selector),
@@ -62,29 +73,27 @@ jQuery.fn.stalker = function (){
         $transitionElems = $stalker.prevAll(":visible");
 
         // need to set overflow to hidden for correct height in IE.
-        function setStalkerHeight () {
 
+        function setStalkerHeight () {
             $stalker.css("overflow", "hidden");
-            stalkerHeight = $stalker.outerHeight();
+            stalkerHeight = $stalker.height();
             $stalker.css("overflow", "");
         }
 
         // create a placeholder as our stalker bar is now fixed
         function createPlaceholder () {
-
             placeholder = jQuery("<div />")
-                .addClass(PLACEHOLDER_CLASS)
+                .addClass("stalker-placeholder")
                 .css({visibility:"hidden", height: stalkerHeight})
                 .insertBefore($stalker);
         }
 
         function setPlaceholderHeight () {
-
-            if (!$stalker.hasClass(DETACHED_CLASS)) {
-                placeholder.height(stalkerHeight);
+            if (!$stalker.hasClass("detached")) {
+                placeholder.height($stalker.height());
             } else {
-                placeholder.height($stalker.removeClass(DETACHED_CLASS).outerHeight());
-                $stalker.addClass(DETACHED_CLASS);
+                placeholder.height($stalker.removeClass("detached").height());
+                $stalker.addClass("detached");
             }
         }
 
@@ -95,25 +104,20 @@ jQuery.fn.stalker = function (){
         // set calculated fixed (or absolute) position
         $stalker.css(getInactiveProperties());
 
-        /**
-         * custom event to reset stalker placeholder height
-         * @event stalkerHeightUpdated
-         */
+        // custom event to reset stalker placeholde r height
         $stalker.bind("stalkerHeightUpdated", setPlaceholderHeight);
-
-         /**
-         * custom event to reset stalker position
-         * @event stalkerHeightUpdated
-         */
         $stalker.bind("positionChanged", setStalkerPosition);
+
+        if (needToApplyFlickerFix()) {
+            $stalker.addClass("fix-ff35-flicker");
+        }
 
         isInitialized = true;
     }
 
-    function offsetKeyboardInitiatedPageScrolling() {
+    function offsetPageScrolling() {
 
         function setScrollPostion(scrollTarget) {
-
             var docHeight = jQuery.getDocHeight(),
                 scrollPos;
             if (scrollTarget >= 0 && scrollTarget <= docHeight) {
@@ -139,6 +143,8 @@ jQuery.fn.stalker = function (){
 
         function pageDown() {
 
+
+
             if (!isInitialized) {
                 initialize();
             }
@@ -149,42 +155,30 @@ jQuery.fn.stalker = function (){
         }
 
         jQuery(function () {
+            $doc.bind('keydown keypress keyup', "pagedown", function (e) {
 
-            $doc.bind('keydown keypress keyup', {combi: "pagedown", disableInInput: true}, function (e) {
-
-                if (!jQuery.browser.mozilla && e.type === "keydown") {
-                    pageDown();
-                } else if (jQuery.browser.mozilla && e.type === "keypress") {
+                if (e.type === "keydown") {
                     pageDown();
                 }
                 e.preventDefault();
             });
 
-            $doc.bind('keydown keypress keyup', {combi: "pageup", disableInInput: true}, function (e) {
-
-                if (!jQuery.browser.mozilla && e.type === "keydown") {
-                    pageUp();
-                } else if (jQuery.browser.mozilla && e.type === "keypress") {
+            $doc.bind('keydown keypress keyup', "pageup", function (e) {
+                if (e.type === "keydown") {
                     pageUp();
                 }
                 e.preventDefault();
             });
 
-            $doc.bind('keydown keypress keyup', {combi: "space", disableInInput: true}, function (e) {
-
-                if (!jQuery.browser.mozilla && e.type === "keydown") {
-                    pageDown();
-                } else if (jQuery.browser.mozilla && e.type === "keypress") {
+            $doc.bind('keydown keypress keyup', "space", function (e) {
+                if (e.type === "keydown") {
                     pageDown();
                 }
                 e.preventDefault();
             });
 
-            $doc.bind('keydown keypress keyup', {combi: "shift+space", disableInInput: true}, function (e) {
-
-                if (!jQuery.browser.mozilla && e.type === "keydown") {
-                    pageUp();
-                } else if (jQuery.browser.mozilla && e.type === "keypress") {
+            $doc.bind('keydown keypress keyup', "shift+space", function (e) {
+                if (e.type === "keydown") {
                     pageUp();
                 }
                 e.preventDefault();
@@ -193,7 +187,6 @@ jQuery.fn.stalker = function (){
     }
 
     function containDropdownsInWindow () {
-
         $doc.bind("showLayer", function (e, type, obj) {
 
             var stalkerOffset,
@@ -204,36 +197,64 @@ jQuery.fn.stalker = function (){
             }
 
             if (type === "dropdown" && obj.$.parents(selector).length !== -1) {
-                stalkerOffset = ($stalker.hasClass(DETACHED_CLASS) ? 0 : $stalker.offset().top);
-                targetHeight = jQuery(window).height() - $stalker.outerHeight() - stalkerOffset;
+                stalkerOffset = ($stalker.hasClass("detached") ? 0 : $stalker.offset().top);
+                targetHeight = jQuery(window).height() - $stalker.height() - stalkerOffset;
                 if (targetHeight <= parseInt(obj.$.attr("scrollHeight"), 10)) {
                     AJS.containDropdown.containHeight(obj, targetHeight);
                 } else {
                     AJS.containDropdown.releaseContainment(obj);
                 }
                 obj.reset();
+
+                /* In firefox 3.5 and below (except on mac) we need to set the stalker to overflow auto to prevent flickering bug when
+                 * scrolling. This has the affect that when a dropdown is opened it overflows the stalker causing scroll bars to
+                 * appear. To prevent this we need to expand the stalker height to its scroll height */
+                if (needToApplyFlickerFix()) {
+                    $stalker.height($stalker.attr("scrollHeight"));
+                }
+            }
+        })
+        .bind("hideLayer", function () {
+            if (needToApplyFlickerFix()) {
+                $stalker.height("");
             }
         });
     }
 
-    function handleAUIPopup () {
-
-        $doc.bind("showLayer", function(e, type) {
-
-            if ($transitionElems) {
-                $transitionElems.css("opacity", "");
-            }
-            // firefox needs to reset the stalker position
-            if (jQuery.browser.mozilla && type === "popup") {
-                setStalkerPosition();
-            }
-        });
+    if (!isSupported()) {
+        return;
     }
+
+    $win = jQuery(window);
+    $doc = jQuery(document);
+    selector = this.selector;
+
+    offsetPageScrolling();
+    containDropdownsInWindow();
+
+    // we may need to update the height of the stalker placeholder, a click event could have caused changes to stalker
+    // height. This should probably be on all events but leaving at click for now for performance reasons.
+    $doc.click(function (e) {
+        if (jQuery(e.target).parents(selector).length !== 0 && !isInitialized) {
+            initialize();
+        }
+    });
+
+    $doc.bind("showLayer", function(e, type) {
+        if ($transitionElems) {
+            $transitionElems.css("opacity", "");
+        }
+        // firefox needs to reset the stalker position
+        if (jQuery.browser.mozilla && type === "popup") {
+            setStalkerPosition();
+        }
+    });
+
+    // offsets perm links, and any anchor's, scroll position so they are offset under ops bar
+    jQuery(".stalker-placeholder, " + this.selector).offsetAnchors();
 
     function setStalkerPosition () {
-
         function getOpacitySetting() {
-
             var opacityTarget = 1 - $win.scrollTop() / offsetY;
             if (opacityTarget > 1) {
                 return "";
@@ -251,66 +272,30 @@ jQuery.fn.stalker = function (){
         $transitionElems.css("opacity", getOpacitySetting());
 
         if (offsetY <= $win.scrollTop()){
-            if (!$stalker.hasClass(DETACHED_CLASS)) {
+            if (!$stalker.hasClass("detached")) {
                 $stalker.css({top:0, position: "fixed"})
-                    .addClass(DETACHED_CLASS);
+                    .addClass("detached");
             }
         } else {
             $stalker.css(getInactiveProperties())
-                .removeClass(DETACHED_CLASS);
+                .removeClass("detached");
         }
         lastScrollPosY = $win.scrollTop();
     }
 
-    function offsetAnchorLinksByStalkerHeight () {
 
-        // offsets perm links, and any anchor's, scroll position so they are offset under ops bar
-        jQuery(".stalker-placeholder, " + this.selector).offsetAnchors();
-    }
-
-    function assignEvents() {
-
-         // we may need to update the height of the stalker placeholder, a click event could have caused changes to stalker
-        // height. This should probably be on all events but leaving at click for now for performance reasons.
-        $doc.click(function (e) {
-            if (!isInitialized && jQuery(e.target).closest(selector).length !== 0) {
-                initialize();
-            }
-        })
-        .mouseup(function () {
-
-            if (lastScrollPosY && $win.scrollTop() === lastScrollPosY) {
-                $transitionElems.css("opacity", "   ");
-            }
-        });
-
-        $win.scroll(setStalkerPosition)
+    $win.scroll(setStalkerPosition)
         .resize(function () {
-
             if ($stalker) {
                 $stalker.trigger("stalkerHeightUpdated");
             }
         });
-    }
 
-    if (!isSupported()) {
-        return;
-    }
-
-    $win = jQuery(window);
-    $doc = jQuery(document);
-    selector = this.selector;
-
-    assignEvents();
-    offsetKeyboardInitiatedPageScrolling();
-    containDropdownsInWindow();
-    handleAUIPopup();
-    offsetAnchorLinksByStalkerHeight();
-
-    // only one instance allowed
-    jQuery.fn.stalker = function () {
-        throw "@method jQuery.fn.stalker: Only one stalker instance allowed, stalker for '" + selector + "' not applied";
-    };
+    $doc.mouseup(function () {
+        if (lastScrollPosY && $win.scrollTop() === lastScrollPosY) {
+            $transitionElems.css("opacity", "   ");
+        }
+    });
 
     return this;
 };
