5b8f53685d7cd2fe87194541bc7a398340ab6ef0
lhardy <lhardy@atlassian.com>
2010-05-13 07:58:20 +0000

    AJS-311: fixes from CR-AJS-219
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@59853 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/main/resources/js/atlassian/tables.js
+++ src/main/resources/js/atlassian/tables.js
@@ -4,7 +4,7 @@
     AJS.Tables.rowStriping = function () {
         var tables = AJS.$("table.aui");
         for (var i=0, ii = tables.length; i < ii; i++) {
-            AJS.$("tbody tr:odd", tables[i]).addClass("odd");
+            AJS.$("tbody tr:odd", tables[i]).addClass("zebra");
         };
     };
     AJS.$(AJS.Tables.rowStriping);
