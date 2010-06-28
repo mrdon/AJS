55b8c67ad568619f012e40eeffe818c860d02773
bwong <bwong@atlassian.com>
2010-06-28 03:23:36 +0000

    AJS-409 added tests for AUI Icons
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@62661 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ auiplugin-tests/src/test/java/it/com/atlassian/aui/javascript/selenium/AUIIconsTest.java
@@ -0,0 +1,41 @@
+package it.com.atlassian.aui.javascript.selenium;
+
+public class AUIIconsTest extends AbstractAUISeleniumTestCase
+{
+    private static final String TEST_PAGE = "test-pages/icons/icons-test.html";
+
+    public void testErrorIcon(){
+        openTestPage(TEST_PAGE);
+        assertThat.elementPresent("css=div#error-icon span.error svg");
+    }
+
+    public void testGenericIcon(){
+        openTestPage(TEST_PAGE);
+        assertThat.elementPresent("css=div#generic-icon span.generic svg");
+    }
+
+    public void testHintIcon(){
+        openTestPage(TEST_PAGE);
+        assertThat.elementPresent("css=div#hint-icon span.hint svg");
+    }
+
+    public void testInfoIcon(){
+        openTestPage(TEST_PAGE);
+        assertThat.elementPresent("css=div#info-icon span.info svg");
+    }
+
+    public void testSucessIcon(){
+        openTestPage(TEST_PAGE);
+        assertThat.elementPresent("css=div#success-icon span.success svg");
+    }
+
+    public void testWarningIcon(){
+        openTestPage(TEST_PAGE);
+        assertThat.elementPresent("css=div#warning-icon span.warning svg");
+    }
+
+        public void testCloseIcon(){
+        openTestPage(TEST_PAGE);
+        assertThat.elementPresent("css=div#close-icon span.close svg");
+    }
+}
