0aa03421d87de3f404b9ee41458ab11f79ea550f
bwong <bwong@atlassian.com>
2010-06-29 07:38:19 +0000

    AJS-409 removing uneccessary println
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@62766 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin-tests/src/test/java/it/com/atlassian/aui/javascript/selenium/AUIUnitTest.java
+++ auiplugin-tests/src/test/java/it/com/atlassian/aui/javascript/selenium/AUIUnitTest.java
@@ -7,7 +7,6 @@ public class AUIUnitTest extends AbstractAUISeleniumTestCase
 
         client.waitForCondition("selenium.isElementPresent('qunit-testresult')");
         int failedTests = Integer.valueOf(client.getEval("window.AJS.$('li.fail li.fail').size()"));
-        System.out.println(failedTests);
         if(failedTests!=0){
             fail("There are " + (failedTests) + " failed unit tests, to debug run: mvn refapp:run in the auiplugin-tests module then go to: http://localhost:9999/ajs/plugins/servlet/ajstest/js/tests/unit/test.html" );
         }
