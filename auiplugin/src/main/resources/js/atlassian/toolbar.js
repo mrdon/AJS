0eed72c4ce98895d306adde7068865bc6d64146a
aro <aro@atlassian.com>
2010-07-07 02:59:31 +0000

    AJS-445 Respond to code review - fix demo page to display html and js source - fix toolbar script to cater for multiple toolbars on a page
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@63228 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin/src/main/resources/js/atlassian/toolbar.js
+++ auiplugin/src/main/resources/js/atlassian/toolbar.js
@@ -2,24 +2,25 @@ AJS.toInit(function($) {
     if ($.browser.msie) {
         // Fix left,right borders on first, last toolbar items
         $(".aui-toolbar .toolbar-group").each(function (i, group) {
-            $(".toolbar-item:first-child", group).addClass("first");
-            $(".toolbar-item:last-child", group).addClass("last");
+            $(group).children(":first").addClass("first");
+            $(group).children(":last").addClass("last");
         });
 
         // Fix right split for IE7
-        if (parseInt($.browser.version) == 7) {
-            var splitRight = $(".aui-toolbar .toolbar-split-right");
-            if (splitRight.length) {
+        if (parseInt($.browser.version, 10) == 7) {
+            $(".aui-toolbar .toolbar-split-right").each(function(i, right) {
+                var splitRight = $(right),
+                    splitLeft = splitRight.closest(".aui-toolbar").find(".toolbar-split-left");
 
-                var splitLeft = $(".aui-toolbar .toolbar-split-left");
-
-                splitRight.css({
-                    "margin-top": "-" + splitLeft.outerHeight()
-                });
-                splitLeft.css({
-                    "padding-right": splitRight.outerWidth()
-                });
-            }
+                if (splitLeft.length) {
+                    splitRight.css({
+                        "margin-top": "-" + splitLeft.outerHeight()
+                    });
+                    splitLeft.css({
+                        "padding-right": splitRight.outerWidth()
+                    });
+                }
+            });
         }
     }    
 });
