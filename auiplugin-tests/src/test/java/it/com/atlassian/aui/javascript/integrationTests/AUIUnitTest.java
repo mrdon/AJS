f993c72ae65782ebd004576b443d09b6d6c4d36a
bwong <bwong@atlassian.com>
2010-07-07 01:47:33 +0000

    adding more information to unit test reports
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@63219 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin-tests/src/test/java/it/com/atlassian/aui/javascript/integrationTests/AUIUnitTest.java
+++ auiplugin-tests/src/test/java/it/com/atlassian/aui/javascript/integrationTests/AUIUnitTest.java
@@ -6,9 +6,10 @@ public class AUIUnitTest extends AbstractAUISeleniumTestCase
         openTestPage("/unit-tests/tests/whenitype.html");
 
         client.waitForCondition("selenium.isElementPresent('qunit-testresult')");
-        int failedTests = Integer.valueOf(client.getEval("window.AJS.$('li.fail li.fail').size()"));
-        if(failedTests!=0){
-            fail("There are " + (failedTests) + " failed unit tests, to debug run: mvn refapp:run in the auiplugin-tests module then go to: http://localhost:9999/ajs/plugins/servlet/ajstest/js/tests/unit/test.html" );
+        int numberOfFailedTests = Integer.valueOf(client.getEval("window.AJS.$('li.fail li.fail').size()"));
+        String failedTests = client.getEval("window.AJS.$('li.fail li.fail')");
+        if(numberOfFailedTests!=0){
+            fail("There are " + (numberOfFailedTests) + " failed unit tests, to debug run: mvn refapp:run in the auiplugin-tests module then go to: http://localhost:9999/ajs/plugins/servlet/ajstest/js/tests/unit/test.html \n "+ failedTests );
         }
 
     }
