d265c006d08e440a12de054740b3cc2cc4e483f0
bbuchanan <bbuchanan@atlassian.com>
2010-07-27 08:00:48 +0000

    AJS-449 AJS-449 active style fixes AJS-456 toolbar wrapping improvement
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@64758 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin/src/main/resources/js/atlassian/toolbar.js
+++ auiplugin/src/main/resources/js/atlassian/toolbar.js
@@ -6,21 +6,18 @@ AJS.toInit(function($) {
             $(group).children(":last").addClass("last");
         });
 
-        // Fix right split for IE7
-        if (parseInt($.browser.version, 10) == 7) {
-            $(".aui-toolbar .toolbar-split-right").each(function(i, right) {
-                var splitRight = $(right),
-                    splitLeft = splitRight.closest(".aui-toolbar").find(".toolbar-split-left");
-
-                if (splitLeft.length) {
-                    splitRight.css({
-                        "margin-top": "-" + splitLeft.outerHeight()
-                    });
-                    splitLeft.css({
-                        "padding-right": splitRight.outerWidth()
-                    });
-                }
-            });
-        }
+        // Fix right split for IE7 - COMMENTED until 100% sure this isn't required
+        //if (parseInt($.browser.version, 10) == 7) {
+        //    $(".aui-toolbar .toolbar-split-right").each(function(i, right) {
+        //        var splitRight = $(right),
+        //            splitLeft = splitRight.closest(".aui-toolbar").find(".toolbar-split-left");
+        //
+        //        if (splitLeft.length) {
+        //            splitLeft.css({
+        //                "padding-right": splitRight.outerWidth()
+        //            });
+        //        }
+        //    });
+        //}
     }    
 });
