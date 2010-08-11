0a46509c588ceca0938465dbf6fe0045a1358d46
andreask <andreask@atlassian.com>
2010-08-11 01:54:53 +0000

    CR-AJS-277: Implemented review comments: * Removed duplicated js files * Added options argument for moveto with sensible defaults * Added a stalker var to avoid traversing the DOM 4 times for it
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@65892 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin/src/main/resources/js/atlassian/jquery/jquery.moveto.js
+++ auiplugin/src/main/resources/js/atlassian/jquery/jquery.moveto.js
@@ -12,20 +12,33 @@
  * jQuery("li.item").moveTo();
  * </pre>
  *
+ * This plugin also supports options as an argument.  The options
+ * that can be defined are:
+ * <ul>
+ * <li>transition - if set to true will cause a smooth scrolling transition (false by default)</li>
+ * <li>scrollOffset - defines an offset to scroll past the element to view in pixels such that
+ * all of it can be viewed (35 pixels by default)</li>
+ * </ul>
+ *
  * @class moveTo
  * @constuctor moveTo
  * @namespace jQuery.fn
- * @param {Boolean} transition
+ * @param {Object} options
  */
-jQuery.fn.moveTo = function (transition) {
+jQuery.fn.moveTo = function (options) {
+    var defaults = {
+        transition: false,
+        scrollOffset: 35
+    };
 
-    var that = this,
-        topOffset = this.offset().top,
-        commandBarHeight = jQuery("#stalker").outerHeight() + 35,
-        scrollTarget,
-        undetachedStalker = jQuery("#stalker.not(.detached)");
+    var opts = jQuery.extend(defaults, options),
+        instance = this,
+        topOffset = instance.offset().top,
+        $stalker = jQuery("#stalker"),
+        commandBarHeight = $stalker.outerHeight() + opts.scrollOffset,
+        scrollTarget;
 
-    if (undetachedStalker.length > 0 && (topOffset <= undetachedStalker.offset().top)) {
+    if (!$stalker.hasClass("detached") && (topOffset <= $stalker.offset().top)) {
         commandBarHeight = 0;
     }
 
@@ -36,21 +49,21 @@ jQuery.fn.moveTo = function (transition) {
 
         if(jQuery(window).scrollTop() + commandBarHeight > topOffset) {
             //move up
-            scrollTarget = topOffset - (jQuery(window).height() - this.outerHeight()) + 35;
+            scrollTarget = topOffset - (jQuery(window).height() - this.outerHeight()) + opts.scrollOffset;
         } else {
             //move down
             scrollTarget = topOffset - commandBarHeight;
         }
 
-        if (!jQuery.fn.moveTo.animating && transition) {
+        if (!jQuery.fn.moveTo.animating && opts.transition) {
             jQuery(document).trigger("moveToStarted", this);
             jQuery.fn.moveTo.animating = true;
             jQuery("html,body").animate({
                 scrollTop: scrollTarget
             }, 1000, function () {
                 // safari needs this to ensure scrollEvent is actually fired
-                jQuery("#stalker").trigger("positionChanged");
-                jQuery(document).trigger("moveToFinished", that);
+                $stalker.trigger("positionChanged");
+                jQuery(document).trigger("moveToFinished", instance);
                 delete jQuery.fn.moveTo.animating;
             });
             return this;
@@ -64,11 +77,11 @@ jQuery.fn.moveTo = function (transition) {
             jQuery(document).trigger("moveToStarted");
             jQuery(window).scrollTop(scrollTarget);
             // safari needs this to ensure scrollEvent is actually fired
-            jQuery("#stalker").trigger("positionChanged");
+            $stalker.trigger("positionChanged");
             //need to put a slight timeout for the moveToFinished event such that recipients of this event
             //have time to act on it.
             setTimeout(function() {
-                jQuery(document).trigger("moveToFinished", that);
+                jQuery(document).trigger("moveToFinished", instance);
             }, 100);
             return this;
         }
