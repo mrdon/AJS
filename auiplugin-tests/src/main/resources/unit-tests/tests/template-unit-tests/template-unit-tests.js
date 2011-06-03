9ec175a614c41cc0d30121944b54acad82bdf136
shughes <shughes@atlassian.com>
2011-06-03 05:55:08 +0000

    AJS-620: Remove unneeded string trim() from template unit test
    git-svn-id: https://studio.atlassian.com/svn/AJS/branches/auiplugin-3.4.x@125220 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin-tests/src/main/resources/unit-tests/tests/template-unit-tests/template-unit-tests.js
+++ auiplugin-tests/src/main/resources/unit-tests/tests/template-unit-tests/template-unit-tests.js
@@ -3,6 +3,6 @@ module("Unit Tests for AJS.template");
 test("html escaping", function() {
     var template = AJS.template("Hello, {name}. Welcome to {application}.<br>");
 
-    equals(template.fill({name:"\"O'Foo\"", application:"<JIRA & Confluence>"}).toString().trim(),
+    equals(template.fill({name:"\"O'Foo\"", application:"<JIRA & Confluence>"}).toString(),
             "Hello, &quot;O&#39;Foo&quot;. Welcome to &lt;JIRA &amp; Confluence&gt;.<br>")
 });
