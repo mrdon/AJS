738fa5d2963fcc5af373c8554a16b2ba744427d7
bbuchanan <bbuchanan@atlassian.com>
2011-06-14 06:05:38 +0000

    AJS-646 changed to different solution for inserting ID, based on review CR-AJS-370. no longer silently correct bad IDs, will fail with log instead.
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@125709 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin-tests/src/main/resources/unit-tests/tests/messages-unit-tests/messages-unit-tests.js
+++ auiplugin-tests/src/main/resources/unit-tests/tests/messages-unit-tests/messages-unit-tests.js
@@ -8,14 +8,6 @@ module("Messages Unit Tests", {
     }
 });
 
-test("Messages API", function() {
-    ok(typeof AJS.messages == "object", "AJS.messages exists");
-    ok(typeof AJS.messages.setup == "function", "AJS.messages.setup exists");
-    ok(typeof AJS.messages.makeCloseable == "function", "AJS.messages.makeCloseable exists");
-    ok(typeof AJS.messages.template == "string", "AJS.messages.template exists");
-    ok(typeof AJS.messages.createMessage == "function", "AJS.messages.createMessage exists");
-});
-
 function messagesSetup(testid) {
     AJS.messages.info({
         id: testid,
@@ -24,25 +16,34 @@ function messagesSetup(testid) {
     });
 }
 
-test("Messages ID test: no hash", function() {
+function checkNoID(target) {
+    return {
+        found: target.find(".aui-message")[0].getAttribute("id"),
+        expected: (AJS.$.browser.msie && ~~(AJS.$.browser.version) < 8) ? "" : null // IE7 needs empty string
+    };
+}
+
+test("Messages API", function() {
+    ok(typeof AJS.messages == "object", "AJS.messages exists");
+    ok(typeof AJS.messages.setup == "function", "AJS.messages.setup exists");
+    ok(typeof AJS.messages.makeCloseable == "function", "AJS.messages.makeCloseable exists");
+    ok(typeof AJS.messages.template == "string", "AJS.messages.template exists");
+    ok(typeof AJS.messages.createMessage == "function", "AJS.messages.createMessage exists");
+});
+
+test("Messages ID test: good ID", function() {
     messagesSetup("test-id-without-hash");
     ok( AJS.$("#test-id-without-hash").length, "#test-id-without-hash should be found" );
 });
 
-test("Messages ID test: with leading hash", function() {
-    messagesSetup("#test-id-with-hash");
-    ok( AJS.$("#test-id-with-hash").length, "#test-id-with-hash should be found" );
-});
-
-test("Messages ID test: with all regex options", function() {
+test("Messages ID test: bad ID", function() {
     messagesSetup("#t.e.st-m### e s s a '''\"\"g e-id-full-of-dodgy-crap");
-    ok( AJS.$("#test-message-id-full-of-dodgy-crap").length, "#test-message-id-full-of-dodgy-crap should be found" );
+    var checkedNoID = checkNoID(this.messagebar);
+    equal( checkedNoID.found, checkedNoID.expected, "There should be no ID (bad ID supplied)");
 });
 
 test("Messages ID test: no ID", function() {
-    // simply omit the ID
     messagesSetup();
-    // IE7 wants an empty string, everything else wants null
-    var nuffin = (AJS.$.browser.msie && ~~(AJS.$.browser.version) < 8) ? "" : null;
-    equal(this.messagebar.find(".aui-message")[0].getAttribute("id"), nuffin, "There should be no ID");
+    var checkedNoID = checkNoID(this.messagebar);
+    equal( checkedNoID.found, checkedNoID.expected, "There should be no ID (no ID supplied)");
 });
