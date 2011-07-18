0d23bd5a8b17896bdeae9adf0f0813760b43fddf
bbuchanan <bbuchanan@atlassian.com>
2011-07-18 08:06:36 +0000

    AJS-667 AJS-569 Make table zebra striping optional and update design to better meet style guide.
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@127109 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin-tests/src/main/resources/unit-tests/tests/tables-unit-tests/tables-unit-tests.js
+++ auiplugin-tests/src/main/resources/unit-tests/tests/tables-unit-tests/tables-unit-tests.js
@@ -1,6 +1,12 @@
 module("Tables Unit Tests");
 
-test("Tables API Test", function() {
-       ok(typeof AJS.tables=="object", "Tables object exists!");
-       ok(typeof AJS.tables.rowStriping=="function", "Tables.rowStripping funciton exists!");
-});
+if (jQuery.browser.msie && jQuery.browser.version < "9") {
+       test("Tables IE8 and below Test", function() {
+              ok(typeof AJS.tables=="object", "Tables object should exist");
+              ok(typeof AJS.tables.rowStriping=="function", "Tables.rowStriping function should exist");
+       });
+} else {
+       test("Tables !IE8 and below Test", function() {
+              ok(typeof AJS.tables=="undefined", "Tables JS should not have loaded");
+       });
+}
--- auiplugin/src/main/resources/js/atlassian/tables.js
+++ auiplugin/src/main/resources/js/atlassian/tables.js
@@ -1,15 +1,16 @@
 /*->
 #name>Tables
-#javascript>No
+#javascript>IE8 and below
 #css>Yes
 #description> Standards Patterns and Styling for HTML Tables
 <-*/
 (function() {
     AJS.tables = AJS.tables || {};
     AJS.tables.rowStriping = function () {
-        var tables = AJS.$("table.aui");
+        var tables = AJS.$("table.aui-zebra");
+        // has to be done this way to restart the count per table       
         for (var i=0, ii = tables.length; i < ii; i++) {
-            AJS.$("tbody tr:odd", tables[i]).addClass("zebra");
+            AJS.$("tbody tr:odd", tables[i]).addClass("aui-zebra");
         };
     };
     AJS.$(AJS.tables.rowStriping);
