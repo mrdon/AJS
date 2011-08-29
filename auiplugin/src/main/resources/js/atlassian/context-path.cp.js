fcaae6c89a0332cdb3ca37dd395a6d8a6d346e1a
shughes <shughes@atlassian.com>
2011-08-29 05:33:48 +0000

    AJS-709: Avoid bug where contextPath = "" is treated as undefined
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@129127 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin/src/main/resources/js/atlassian/context-path.cp.js
+++ auiplugin/src/main/resources/js/atlassian/context-path.cp.js
@@ -4,25 +4,33 @@ AJS = AJS || {};
     // don't change this line, they are the target of a search/replace
     var AJS_SUBSTITUTE_CONTEXT_PATH_HERE = false;
 
+    var contextPath = null;
+    var paths = [
+        AJS_SUBSTITUTE_CONTEXT_PATH_HERE,
+        window.contextPath, // JIRA
+        window.Confluence && Confluence.getContextPath(), // Confluence
+        window.BAMBOO && BAMBOO.contextPath, // Bamboo
+        window.FECRU && FECRU.pageContext // FishEye/Crucible
+    ];
+    for (var i = 0; i < paths.length; i++) {
+        if (typeof paths[i] === "string") {
+            contextPath = paths[i];
+            break;
+        }
+    }
+
     /**
      * Gives the context-path for this application. The returned string will never
      * end with a slash, and will either be the empty string, or start with slash.
      *
      * Example values: "", "/jira", "/some/large/context"
      *
+     * @return {?string} -- A null return value indicates that no context path
+     *                      information was discovered.
      * @since 3.5
      */
     AJS.contextPath = function() {
-        var result = AJS_SUBSTITUTE_CONTEXT_PATH_HERE ||
-                // fallback to product-specific constants
-                window.contextPath || // JIRA
-                (window.Confluence && Confluence.getContextPath())|| // confluence
-                (window.BAMBOO && BAMBOO.contextPath) || // bamboo
-                (window.FECRU && FECRU.pageContext); // fisheye/crucible
-
-        AJS.contextPath = function() { return result; }; // self-replace
-        
-        return result;
+        return contextPath;
     };
 
 })();
