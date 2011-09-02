263f00444c63fe2f7806f906e767f1e9a9b17f31
shughes <shughes@atlassian.com>
2011-09-02 05:35:19 +0000

    AJS-709: Re-evaluate contextPath each time AJS.contextPath() is called
    git-svn-id: https://studio.atlassian.com/svn/AJS/trunk@129319 2c54a935-e501-0410-bc05-97a93f6bca70

--- auiplugin/src/main/resources/js/atlassian/context-path.cp.js
+++ auiplugin/src/main/resources/js/atlassian/context-path.cp.js
@@ -4,21 +4,6 @@ AJS = AJS || {};
     // don't change this line, they are the target of a search/replace
     var AJS_SUBSTITUTE_CONTEXT_PATH_HERE = false;
 
-    var contextPath = null;
-    var paths = [
-        AJS_SUBSTITUTE_CONTEXT_PATH_HERE,
-        window.contextPath, // JIRA
-        window.Confluence && Confluence.getContextPath(), // Confluence
-        window.BAMBOO && BAMBOO.contextPath, // Bamboo
-        window.FECRU && FECRU.pageContext // FishEye/Crucible
-    ];
-    for (var i = 0; i < paths.length; i++) {
-        if (typeof paths[i] === "string") {
-            contextPath = paths[i];
-            break;
-        }
-    }
-
     /**
      * Gives the context-path for this application. The returned string will never
      * end with a slash, and will either be the empty string, or start with slash.
@@ -30,6 +15,20 @@ AJS = AJS || {};
      * @since 3.5
      */
     AJS.contextPath = function() {
+        var contextPath = null;
+        var paths = [
+            AJS_SUBSTITUTE_CONTEXT_PATH_HERE,
+            window.contextPath, // JIRA
+            window.Confluence && Confluence.getContextPath(), // Confluence
+            window.BAMBOO && BAMBOO.contextPath, // Bamboo
+            window.FECRU && FECRU.pageContext // FishEye/Crucible
+        ];
+        for (var i = 0; i < paths.length; i++) {
+            if (typeof paths[i] === "string") {
+                contextPath = paths[i];
+                break;
+            }
+        }
         return contextPath;
     };
 
